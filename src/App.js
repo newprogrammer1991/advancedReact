6fcfb74700e5c448505958a907905d2a945b4a35
Ali <ali.kholmatov@gmail.com>
2018-02-13 16:06:57 +0300

    initial config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
-
-
+import Root from './components/Root'
+import store from './redux'
+import {Provider} from 'react-redux'
+import {ConnectedRouter} from 'react-router-redux'
+import history from './history'
 class App extends Component {
     render() {
         return (
-            <div>Hello</div>
+            <Provider store={store}>
+                <ConnectedRouter history={history}>
+                    <Root/>
+                </ConnectedRouter>
+            </Provider>
         );
     }
 }
---
+++ src/redux/reducer.js
@@ -0,0 +1,5 @@
+import {combineReducers} from 'redux'
+import {routerReducer as router} from 'react-router-redux'
+export default combineReducers({
+    router
+})
